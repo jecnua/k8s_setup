a8a19513a4f0b780b73bb98c12d0d278d40e2968
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-02-01 11:05:05 +0000

    Added on the run command

--- 01-scope/run.sh
+++ 01-scope/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 kubectl apply -f 00-weavescope_list.yaml
+kubectl apply -f 01-weavescope_svc.yaml
 kubectl get ds --all-namespaces | grep weavescope
 kubectl get svc --all-namespaces | grep weavescope
