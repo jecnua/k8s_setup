c62190df7672bbc5ce17ca67a1ddbe49cc638f25
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-02-01 11:27:43 +0000

    Changed file name

--- 03-fluentd/run.sh
+++ 03-fluentd/run.sh
@@ -5,7 +5,7 @@
 kubectl create configmap es-config --from-file=config/ --namespace=kube-system
 kubectl apply -f 01b-es_rc_5.x.yaml
 kubectl get pods --all-namespaces | grep elastic
-kubectl logs elasticsearch-logging-v1-87nwz -n kube-system
+# kubectl logs elasticsearch-logging-v1-87nwz -n kube-system
 #  kubectl exec -it elasticsearch-logging-v1-87nwz -n kube-system -- curl localhost:9200
 #  kubectl exec -it elasticsearch-logging-v1-87nwz -n kube-system -- curl localhost:9200/_cat/indices
 kubectl apply -f 02-es_svc.yaml
