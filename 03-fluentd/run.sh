563260c41ff69370fce8b9e1b32193c77ee0a432
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-02-01 11:26:41 +0000

    Changing path of fluentd config

--- 03-fluentd/run.sh
+++ 03-fluentd/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd configs/ || exit
-# kubectl apply -f 01-*.yaml
-kubectl create configmap fluentd-config --from-file=fluent.conf --namespace=kube-system
+# kubectl create configmap fluentd-config --from-file=fluent.conf --namespace=kube-system
+
+kubectl create configmap es-config --from-file=config/ --namespace=kube-system
+kubectl apply -f 01b-es_rc_5.x.yaml
+kubectl get pods --all-namespaces | grep elastic
+kubectl logs elasticsearch-logging-v1-87nwz -n kube-system
+#  kubectl exec -it elasticsearch-logging-v1-87nwz -n kube-system -- curl localhost:9200
+#  kubectl exec -it elasticsearch-logging-v1-87nwz -n kube-system -- curl localhost:9200/_cat/indices
+kubectl apply -f 02-es_svc.yaml
+kubectl create configmap fluentd-config --from-file=fluentd_config/fluent.conf --namespace=kube-system
