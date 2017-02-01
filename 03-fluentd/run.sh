41fc8f7ab297994047a24933c4f2239505202d08
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-02-01 11:43:06 +0000

    Fluentd run updated

--- 03-fluentd/run.sh
+++ 03-fluentd/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# kubectl create configmap fluentd-config --from-file=fluent.conf --namespace=kube-system
-
 kubectl create configmap es-config --from-file=config/ --namespace=kube-system
 kubectl apply -f 01b-es_rc_5.x.yaml
 kubectl get pods --all-namespaces | grep elastic
 # kubectl logs elasticsearch-logging-v1-87nwz -n kube-system
-#  kubectl exec -it elasticsearch-logging-v1-87nwz -n kube-system -- curl localhost:9200
-#  kubectl exec -it elasticsearch-logging-v1-87nwz -n kube-system -- curl localhost:9200/_cat/indices
+sleep 5
+# kubectl exec -it elasticsearch-logging-xxx -n kube-system -- curl localhost:9200
 kubectl apply -f 02-es_svc.yaml
 kubectl create configmap fluentd-config --from-file=fluentd_config/fluent.conf --namespace=kube-system
+kubectl apply -f 03-fluentd_ds.yaml
+sleep 5
+# kubectl exec -it elasticsearch-logging-xxx -n kube-system -- curl localhost:9200/_cat/indices
+kubectl apply -f 04-kibana_deploy.yaml
+kubectl apply -f 05-kibana_svc.yaml
