aa4f00bbadc6001c666fe751227a44eb94fb38a5
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-02-01 11:13:19 +0000

    Small change in run

--- 01-scope/run.sh
+++ 01-scope/run.sh
@@ -3,6 +3,5 @@
 kubectl apply -f 00-weavescope_list.yaml
 kubectl apply -f 01-weavescope_svc.yaml
 kubectl get ds --all-namespaces | grep 'weave-scope-agent'
-kubectl get svc --all-namespaces | grep 'weave-scope-app'
 kubectl get svc --all-namespaces | grep 'weave-scope-app-elb'
 kubectl describe svc 'weave-scope-app-elb'
