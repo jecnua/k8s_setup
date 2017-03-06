1a430258e5744d56a6e2eeb4fe49eba7c0a907ba
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-03-06 17:38:55 +0000

    Added weave scope and helm install

---
+++ __helm/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+
+VERSION='2.1.3'
+if [ $# -ne 0 ]
+then
+  VERSION='$1'
+fi
+
+cd /opt || exit
+wget "https://kubernetes-helm.storage.googleapis.com/helm-v$VERSION-linux-amd64.tar.gz"
+mkdir -p "helm-v$VERSION"
+tar -zxvf "helm-v$VERSION-linux-amd64.tar.gz" -C "helm-v$VERSION"
+if [[ -f /opt/"helm-v$VERSION"/linux-amd64/helm ]]
+then
+  ln -fs /opt/"helm-v$VERSION"/linux-amd64/helm /usr/local/bin/helm
+else
+  echo "File to link not found"
+fi
