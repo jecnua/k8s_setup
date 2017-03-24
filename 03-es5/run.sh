cb5a93ef4f7a241ffe6c3e61009371fbfd6099a2
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-03-24 17:14:35 +0000

    Cleaned up some more

--- 03-es5/run.sh
+++ 03-es5/run.sh
@@ -2,7 +2,6 @@
 
 kubectl create configmap es-config --from-file=config/ --namespace=kube-system
 kubectl apply -f 01-es_rc.yaml
-kubectl get pods --all-namespaces | grep elastic
 kubectl apply -f 02-es_svc.yaml
-kubectl apply -f 04-kibana_deploy.yaml
-kubectl apply -f 05-kibana_svc.yaml
+kubectl apply -f 03-kibana_deploy.yaml
+kubectl apply -f 04-kibana_svc.yaml
