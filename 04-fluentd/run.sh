c908f5dcaadbb60d4e1ad8e130b3b6f4eb06f3b3
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-03-24 17:28:12 +0000

    Fixed old reference

--- 04-fluentd/run.sh
+++ 04-fluentd/run.sh
@@ -3,4 +3,4 @@
 kubectl create configmap fluentd-config \
   --from-file=fluentd_config/fluent.conf \
   --namespace=kube-system
-kubectl apply -f 03-fluentd_ds.yaml
+kubectl apply -f 01-fluentd_ds.yaml
