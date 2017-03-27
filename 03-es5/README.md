b8b6c672b7ef95ca433f991801eac1dc0a21bca1
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-03-27 15:56:22 +0100

    Removed old file. Added stuff on README

--- 03-es5/README.md
+++ 03-es5/README.md
@@ -1,5 +1,10 @@
 # ES 5 for logging
 
+Get all data:
+
+    kubectl get ns,pv && \
+      kubectl get cm,pvc,statefulset,pods,deploy,svc -n logs
+
 ## TODO
 
 - Data (Volume) is not encrypted
--- 03-es5/run.sh
+++ 03-es5/run.sh
@@ -5,11 +5,9 @@ kubectl create configmap es-config \
   --from-file=config/ \
   --namespace=logs
 kubectl apply -f 01-es_ss.yaml
-# kubectl apply -f 02-es_svc.yaml
-# kubectl apply -f 03-kibana_deploy.yaml
-# kubectl apply -f 04-kibana_svc.yaml
+kubectl apply -f 02-es_svc.yaml
+kubectl apply -f 03-kibana_deploy.yaml
+kubectl apply -f 04-kibana_svc.yaml
 
-
-kubectl get ns
-kubectl get pv
-kubectl get cm,pvc,statefulset,pods -n logs
+kubectl get ns,pv && \
+  kubectl get cm,pvc,statefulset,pods,deploy,svc -n logs
