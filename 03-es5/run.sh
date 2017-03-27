6877abbe068b607687ca2bd1468bf5ffc72cdfeb
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-03-27 15:38:20 +0100

    Changed all reference to namespace. Using now logs namespace.

--- 03-es5/run.sh
+++ 03-es5/run.sh
@@ -2,8 +2,8 @@
 
 kubectl create configmap es-config \
   --from-file=config/ \
-  --namespace=kube-system
-kubectl apply -f 01-es_rc.yaml
-kubectl apply -f 02-es_svc.yaml
-kubectl apply -f 03-kibana_deploy.yaml
-kubectl apply -f 04-kibana_svc.yaml
+  --namespace=logs
+# kubectl apply -f 01-es_rc.yaml
+# kubectl apply -f 02-es_svc.yaml
+# kubectl apply -f 03-kibana_deploy.yaml
+# kubectl apply -f 04-kibana_svc.yaml
