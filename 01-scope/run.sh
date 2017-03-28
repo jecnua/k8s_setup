e25724f51933d2b0d269ef90572dea8ac4db01e3
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-03-28 12:54:08 +0100

    Fixed typo

--- 01-scope/run.sh
+++ 01-scope/run.sh
@@ -3,4 +3,4 @@
 kubectl apply -f 00-weavescope_list.yaml
 kubectl apply -f 01-weavescope_svc.yaml
 kubectl get ds --all-namespaces | grep 'weave-scope-agent'
-kubectl describe svc 'weave-scope-app-elb'
+kubectl describe svc 'scope-np'
