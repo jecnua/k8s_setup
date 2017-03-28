742c4a4b7f9ba1473b9ef8acbb72b3ae9bf9acb4
Fabrizio Sabatini <fabrizio.sabatini.it@gmail.com>
2017-03-28 12:58:19 +0100

    Removed my scope svc

--- 01-scope/run.sh
+++ 01-scope/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 kubectl apply -f 00-weavescope_list.yaml
-kubectl apply -f 01-weavescope_svc.yaml
 kubectl get ds --all-namespaces | grep 'weave-scope-agent'
-kubectl describe svc 'scope-np'
+kubectl describe svc 'weave-scope-app'
